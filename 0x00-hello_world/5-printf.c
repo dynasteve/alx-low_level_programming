35d9c999e5b4ef71568f635e5195882862427f4e
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-08 18:48:14 +0100

    C Hello World Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  *main - print a string
  *Description: Using the main function
- *This program prints with proper grammar, but the outcome is a piece of art
+ *This program prints with proper grammar, but the outcome is a piece of art,
  *Return: 0
  */
 #include <string.h>
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
