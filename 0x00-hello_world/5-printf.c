8fb3ce0b0f4e679e543d5455c5d43ed8e00b95fe
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-08 18:47:10 +0100

    C Hello World Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - print a string
+ *Description: Using the main function
+ *This program prints with proper grammar, but the outcome is a piece of art
+ *Return: 0
+ */
+#include <string.h>
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
