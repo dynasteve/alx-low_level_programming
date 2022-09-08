5f9033dbbdd33d612d06ff39ca5722d4dcd41701
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-08 21:27:51 +0100

    C Hello World Advanced task 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char m[100]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char m[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fprintf(stderr, "%s", m);
 return (0);
 }
