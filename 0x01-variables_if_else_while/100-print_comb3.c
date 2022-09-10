da4edc91d2cfddcb1a3f435fab59185d0fb0a86f
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-10 02:00:09 +0100

    C - Variables, if, else, while Advanced task 0

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,31 +8,30 @@
  */
 int main(void)
 {
-	int i;
-	int j;
-	int comma = ',';
-	int space = ' ';
+int i;
+int j;
+int comma = ',';
+int space = ' ';
 
-	for (i = '0'; i <= '8'; i++)
-	{
-		for (j = '1'; j <= '9'; j++)
-		{
-			if (i < j)
-			{
-				putchar(i);
-				putchar(j);
+for (i = '0'; i <= '8'; i++)
+{
+for (j = '1'; j <= '9'; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
 
-				if (i < '8' || j < '9')
-				{
-					putchar(comma);
-					putchar(space);
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+if (i < '8' || j < '9')
+{
+putchar(comma);
+putchar(space);
+}
+}
+}
 }
 
+putchar('\n');
+
+return (0);
+}
