a0ca9806e7bfff3b4f2450fb158e1ae365062a59
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-10 01:18:45 +0100

    C - Variables, if, else, while task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ *main - prints 1 tp 10
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
+ */
+int main(void)
+{
+for(int i = 0; i <= 10; i++)
+{
+printf("%d", i);
+}
+}
