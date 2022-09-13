528b445a4c6c300977872cf622e1187ab39271bd
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-13 23:38:23 +0100

    Functions and nested loops task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints using _putchar to stdout
+ *Description: Using the print_alphabet_x10 function
+ *This program prints a - z 10 times
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+int x;
+int y;
+
+for (x = 1; x <= 10; x++){
+for (y = 'a'; y <= 'z'; y++)
+{
+_putchar(y);
+}
+_putchar(\n);
+}
+}
