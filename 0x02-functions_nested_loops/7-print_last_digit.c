00a53b06c628bf84c15625298912e0888e7ffaa8
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 00:30:36 +0100

    Functions and nested loops task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints using _putchar to stdout
+ *Description: Using the print_last_digit function for args n
+ *@n: Is the used arguement
+ *This program prints last digit of an interger
+ *Return: 0
+ */
+
+int print_last_digit(int n)
+{
+int x = n % 10;
+return (x);
+}
