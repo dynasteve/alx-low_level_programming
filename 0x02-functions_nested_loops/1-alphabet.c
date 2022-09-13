12261da78f0dcea45dc125c6a2abb66fe09cb7fb
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-13 23:30:31 +0100

    Functions and nested loops task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *main - prints _putchar to stdout
- *Description: Using the main function
+ *Description: Using the print_alphabet function
  *This program prints a - z
  *Return: 0
  */
@@ -9,7 +9,7 @@
 void print_alphabet(void)
 {
 char chara;
-for(chara = 'a'; chara <= 'z'; chara++)
+for (chara = 'a'; chara <= 'z'; chara++)
 {
 _putchar(chara);
 }
