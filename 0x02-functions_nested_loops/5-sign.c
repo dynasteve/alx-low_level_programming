0a540ca9b68135c851f75a45b1bab4f3605c638f
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 00:18:48 +0100

    Functions and nested loops task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_sign - prints using _putchar to stdout
+ *Description: Using the print_sign function for args n
+ *@n: Is the used arguement
+ *This program prints positive or negative prompts
+ *Return: 0
+ */
+
+print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+else if (n < 0)
+{
+return (-1);
+_putchar('-');
+}
+else
+{
+return (0);
+_putchar('0');
+}
+}
