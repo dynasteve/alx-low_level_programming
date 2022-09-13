29e75b95303295e875322e1ea7a2bc8f1fc1742b
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 00:26:45 +0100

    Functions and nested loops task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - prints using _putchar to stdout
+ *Description: Using the _abs function for args n
+ *@n: Is the used arguement
+ *This program prints absolute value of integer
+ *Return: 0
+ */
+
+int _abs(int x)
+{
+return (abs(x));
+}
