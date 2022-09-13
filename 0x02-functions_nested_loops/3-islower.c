0afed84e8e06a33180a419b7616e9671de08b458
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-13 23:55:43 +0100

    Functions and nested loops task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints using _putchar to stdout
+ *Description: Using the print_alphabet_x10 function
+ *This program prints a - z 10 times
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+    if (_islower(c) != 0)
+    {
+        return('1');
+    }
+    else (_islower(c) == 0)
+    {
+        return('0');
+    }
+}
