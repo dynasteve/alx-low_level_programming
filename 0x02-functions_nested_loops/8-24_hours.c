66aa6dbc9178c7ecc34e44a51693a91e9a300206
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 00:57:25 +0100

    Functions and nested loops task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints using _putchar to stdout
+ *Description: Using the jack_bauer function for args n
+ *This program prints All minutes of Jack Bauer
+ *Return: 0
+ */
+
+void jack_bauer(void)
+{
+int a;
+int b;
+int c;
+int d;
+
+for (a = 0; a <= 2; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+for(c = 0; c <= 6; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+_putchar(d);
+}
+_putchar(c);
+}
+_putchar(b);
+}
+_putchar(a);
+}
+}
