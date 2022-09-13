c9b067638d4bae44d69ee9b4944151bc5e8f3995
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-13 21:15:30 +0100

    Functions and nested loops task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - prints _putchar
+ *Description: Using the main function
+ *This program prints to stderr
+ *Return: 0
+ */
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
