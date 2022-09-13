69380e6594490b11a4dc4ce219774f6f85aef283
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 00:07:39 +0100

    Functions and nested loops task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 #include <ctype.h>
 /**
  *_islower - prints using _putchar to stdout
- *Description: Using the _islower function
+ *Description: Using the _islower function for c
+ *@c: Is the used arguement
  *This program prints 0 or 1 for letters
  *Return: 0
  */
@@ -11,10 +12,10 @@ int _islower(int c)
 {
 if (islower(c) == 0)
 {
-return(0);
+return (0);
 }
 else
 {
-return(1);
+return (1);
 }
 }
