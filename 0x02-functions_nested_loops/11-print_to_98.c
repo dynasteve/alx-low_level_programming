e0eb5508a3f628f7d67fd3f8fe506fa0a4605d1b
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 03:12:53 +0100

    Functions and nested loops task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,24 @@ void print_to_98(int n)
 {
 if (n > 98)
 {
-while(n >= 98)
-{
+
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
+
+while(n >= 97)
+{
 _putchar(',');
 _putchar(' ');
+
+if (n <= 9)
+{
+_putchar(' ');
+}
+else
+{
+_putchar(((n + 1) / 10) + '0');
+}
+_putchar(((n + 1) % 10) + '0');
 n--;
 }
 }
