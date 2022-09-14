16cc611ec30e2fb5ff4f42f1ca1018442b933b1b
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 01:02:21 +0100

    Functions and nested loops task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,13 +14,13 @@ int b;
 int c;
 int d;
 
-for (a = 0; a <= 2; a++)
+for (a = '0'; a <= '2'; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = '0'; b <= '9'; b++)
 {
-for(c = 0; c <= 6; c++)
+for(c = '0'; c <= '6'; c++)
 {
-for (d = 0; d <= 9; d++)
+for (d = '0'; d <= '9'; d++)
 {
 _putchar(d);
 }
