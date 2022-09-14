7c8ae64df5c05737c2962364b077be910b5e0c70
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 02:07:13 +0100

    Functions and nested loops task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *print_to_98 - prints using _putchar to stdout
+ *Description: Using the print_to_98 function
+ *This program prints the numbers from n to 98
+ *@n : First arguement
+ *Return: 0
+*/
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+while(n >= 98)
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+n--;
+}
+}
+else if (n < 98)
+{
+while(n <= 98)
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+n++;
+}
+}
+
+else
+{
+_putchar(n);
+}
+}
