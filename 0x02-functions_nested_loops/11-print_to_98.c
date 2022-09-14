a89c54a4b7897317d4cfd8681e18b367ea0dc61d
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 04:12:58 +0100

    Functions and nested loops task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,45 +3,45 @@
 
 /**
  *print_to_98 - prints using _putchar to stdout
- *Description: Using the print_to_98 function
- *This program prints the numbers from n to 98
- *@n : First arguement
- *Return: 0
+*Description: Using the print_to_98 function
+*This program prints the numbers from n to 98
+*@n : First arguement
+*Return: 0
 */
 
 void print_to_98(int n)
 {
-    int upper = 98;
+int upper = 98;
 
-	if (n > upper)
-	{
-		while (n >= upper)
-		{
-			if (n != upper)
-			{
-				printf("%d%s", n, ", ");
-			}
-			else
-			{
-				printf("%d", n);
-			}
-			n--;
-		}
-	}
-	else
-	{
-		while (n <= upper)
-		{
-			if (n != upper)
-			{
-				printf("%d%s", n, ", ");
-			}
-			else
-			{
-				printf("%d", n);
-			}
-			n++;
-		}
-	}
-	putchar(10);
+if (n > upper)
+{
+while (n >= upper)
+{
+if (n != upper)
+{
+printf("%d%s", n, ", ");
+}
+else
+{
+printf("%d", n);
+}
+n--;
+}
+}
+else
+{
+while (n <= upper)
+{
+if (n != upper)
+{
+printf("%d%s", n, ", ");
+}
+else
+{
+printf("%d", n);
+}
+n++;
+}
+}
+putchar(10);
 }
