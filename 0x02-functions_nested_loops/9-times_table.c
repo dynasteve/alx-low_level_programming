7acbff0908af1ec40aa2815ec09b2dc6ee3dd78d
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 01:44:08 +0100

    Functions and nested loops task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,37 @@
  *Description: Using the times_table function
  *This program prints the multiplication table till 9
  *Return: 0
- */
+*/
 
 void times_table(void)
 {
-int number;
+int num;
 int multiplier;
+int result;
 
-for (number = '0'; number <= '9'; number++)
+for (num = 0; num <= '0'; num++)
 {
-for (multiplier = '0'; multiplier <= '9'; multiplier++)
-{
-_putchar(number * multiplier);
-if (multiplier != '9')
+_putchar(0);
+
+for (multiplier = 0; multiplier <= 0; multiplier++)
 {
 _putchar(',');
+_putchar(' ');
+
+result = num * multiplier;
+
+if (result != 9)
+{
 _putchar(' ');
 }
 else
 {
-_putchar('$');
+_putchar((result / 10) + '0');
+}
+
+_putchar((result % 10) + '0');
+}
+
 _putchar('\n');
-}
-}
 }
 }
