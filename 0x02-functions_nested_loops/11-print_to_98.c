ff87db866cedef66e9a12efe5faa50b727bdacc9
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 03:58:18 +0100

    Functions and nested loops task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,7 +21,7 @@ void print_to_98(int n)
             for (unit = n % 10; unit <= 9; unit++)
             {
                 _putchar(ten + '0');
-                _putchar(unit + '0');
+                _putchar((n % 10) + '0');
                 _putchar(',');
                 _putchar(' ');
                 n++;
