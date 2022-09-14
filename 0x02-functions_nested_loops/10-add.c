bd8d1550a445b5d4ddcb7b8a207858291f7adf5a
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-14 01:58:50 +0100

    Functions and nested loops task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ *add - prints using _putchar to stdout
+ *Description: Using the add function
+ *This program prints the addition of two intergers
+ *@x : First arguement
+ *@y : Second arguement
+ *Return: 0
+*/
+
+int add(int x, int y)
+{
+return (x + y);
+}
