80fb100daf7860dcb01d55bfd4f9613c4eb1586b
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-22 00:30:09 +0100

    Uppercasing

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - Capitalizes strings
+ * @str: Array
+ * Return: returns string
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if(str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+
+return (str);
+}
