d402db2dbeb5bdb545fa16144e9ee1f516f705ca
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-21 23:29:00 +0100

    Comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Conactinates two strings
+ * @s1: character value.
+ * @s2: character value
+ * Return: returns pointer of @dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int result = strcmp(s1, s2);
+return (result);
+}
