84fb0e7d72d0405df10c0988b5d17323e74a1bb0
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-21 23:02:36 +0100

    Copying Strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Conactinates two strings
+ * @dest: character value.
+ * @src: character value
+ * @n: size of memory
+ * Return: returns pointer of @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
