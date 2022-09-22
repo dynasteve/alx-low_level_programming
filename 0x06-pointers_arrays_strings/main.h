f6f5e225cbd0a4af82b25598a7e5d41fdf25f4a4
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-22 22:37:39 +0100

    Advanced tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+void print_buffer(char *b, int size);
 #endif /*MAIN_H*/
