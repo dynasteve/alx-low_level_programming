8eb40bb9fdc6d4648fc71b46ec6c2622b89c2652
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-28 20:27:39 +0100

    print string len

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen_recursion - Prints a string length
+  * @s: the string
+  *
+  * Return: Nothing.
+  */
+
+int _strlen_recursion(char *s)
+{
+int x = strlen(s);
+
+printlen(x);
+}
+
+/**
+  * printlen - Prints a string length
+  * @x: integer length of the string
+  *
+  * Return: Nothing.
+  */
+
+void printlen(int *x)
+{
+char len[];
+int *ptr = NULL;
+sprintf(len, x);
+
+ptr = len;
+
+if (x > 0)
+{
+_putchar(*(ptr + x--))
+}
+else
+{
+x = 0;
+}
+}
