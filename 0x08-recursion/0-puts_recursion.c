171db67f98e9e8158d548957da8cf0219333ca6c
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-28 16:46:54 +0100

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts_recursion: Prints a string
+  *
+  * @s: the string
+  *
+  * Return: Nothing.
+  */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
