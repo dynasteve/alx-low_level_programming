80d56ef38b9af83733b996f5ad5e1846cedc2ea9
Stephen Akande <tosisiyesteve@gmail.com>
2022-09-28 17:59:26 +0100

    print string rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  * _print_rev_recursion - Prints a string in reverse
+  *
+  * @s: the string
+  *
+  * Return: Nothing.
+  */
+void reversing(char *a, int x);
+void _print_rev_recursion(char *s)
+{
+int x = strlen(s);
+reversing(s, x);
+}
+
+void reversing(char *a, int x)
+{
+int *ptr = NULL;
+ptr = a;
+int i;
+for(i = x - 1; i < 0; i--)
+{
+printf("%d", *(ptr + i));
+}
+}
